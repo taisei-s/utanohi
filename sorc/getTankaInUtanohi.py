3d3401b8e9a7d9e229da6173517ec9dc7d7b99e5
taisei sato <taisei-s@h28is019.naist.jp>
2019-06-21 00:36:25 +0900

    add def getAllTanka, method add_file

--- sorc/getTankaInUtanohi.py
+++ sorc/getTankaInUtanohi.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import html5lib
 import time
 import re
-from manager import CsvManager
+from manager import CsvManager, JsonManager, TxtManager
 
 def getArticleOfUtanohi(day, id_, pagename):
     """
@@ -66,17 +66,25 @@ def getDataInUtanohi(days, first_day=1):
     for i in range(len(themes)):
         data.append([themes[i]] + tankas[i])
 
-    for d in data:
-        print(d)
     return data
+
+def getAllTanka():
+    #437, 518, 978
+    today = 1904
+    data = getDataInUtanohi(365)
+    csv_manager = CsvManager()
+    csv_manager.make_file(data, '../data/theme_tanka.csv')
+
+    for day in range(366, today, 365):
+        last_day = day + 365 - 1
+        if last_day > today:
+            last_day = today
+        data = getDataInUtanohi(last_day, first_day=day)
+        csv_manager.add_file(data, '../data/theme_tanka.csv')
 
 
 def main():
-    #437, 518, 978
-    days = 978
-    csv_manager = CsvManager()
-    data = getDataInUtanohi(days, first_day=978)
-    #csv_manager.make_file(data, '../data/theme_tanka.csv')
+    getAllTanka()
 
 if __name__ == "__main__":
     main()
--- sorc/manager.py
+++ sorc/manager.py
@@ -17,23 +17,34 @@ class Manager(metaclass=ABCMeta):
 
 class TxtManager(Manager):
     def make_file(self, data, filename):
-        with open(filename, 'w') as txtfile:
-            if type(data) is list:
+        if type(data) is list:
+            with open(filename, 'w') as txtfile:
                 txtfile.writelines(data)
-            elif type(data) is str:
+        elif type(data) is str:
+            with open(filename, 'w') as txtfile:
                 txtfile.write(data)
-    
+        else:
+            raise AttributeError("data type '{}' can't write file in this method. please use data type str or list".format(type(data)))
+
     def load_file(self, filename):
         with open(filename, 'r') as txtfile:
             text = txtfile.readlines()
-        
         return text
+
+    def add_file(self, data, filename):
+        if type(data) is list:
+            with open(filename, 'a') as txtfile:
+                txtfile.writelines(data)
+        elif type(data) is str:
+            with open(filename, 'a') as txtfile:
+                txtfile.write(data)
+        else:
+            raise AttributeError("data type '{}' can't write file in this method. please use data type str or list".format(type(data)))
 
 class JsonManager(Manager):
     def make_file(self, data, filename):
         if type(data) is not dict:
-            print("type error")
-            exit
+            raise AttributeError("data type '{}' can't write file in this method. please use data type dict".format(type(data)))
 
         with open(filename, 'w') as jsonfile:
             json.dump(data, jsonfile)
@@ -41,23 +52,40 @@ class JsonManager(Manager):
     def load_file(self, filename):
         with open(filename, 'r') as jsonfile:
             json_dict = json.load(jsonfile)
-
         return json_dict
 
 
 class CsvManager(Manager):
     def make_file(self, data, filename):
         if type(data) is not list:
-            print("type error")
-            exit
+            raise AttributeError("data type '{}' can't write file in this method. please use data type list".format(type(data)))
 
         with open(filename, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerows(data)
+            if isinstance(data[0], list) is False:
+                writer.writerow(data)
+            else:
+                writer.writerows(data)
 
     def load_file(self, filename):
         with open(filename, 'r', newline='') as csvfile:
             reader = csv.reader(csvfile)
             csv_list = [row for row in reader]
-
         return csv_list
+
+    def add_file(self, data, filename):
+        if type(data) is not list:
+            raise AttributeError("data type '{}' can't write file in this method. please use data type list".format(type(data)))
+
+        with open(filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            if isinstance(data[0], list) is False:
+                writer.writerow(data)
+            else:
+                writer.writerows(data)
+
+
+if __name__ == "__main__":
+    test = [[1, 2, 3, 4], [1, 2, 3, 4]]
+    csvtest = CsvManager()
+    csvtest.add_file(test, 'test.csv')

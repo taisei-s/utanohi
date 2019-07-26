11c9e7b8c8aba1923a1f2842b30a8118157a3719
taisei-s <suger60.t@gmail.com>
2019-07-26 10:43:42 +0900

    complete merge and creategraph

--- sorc/parseFixed.py
+++ sorc/parseFixed.py
@@ -154,6 +154,113 @@ def parseFixed(tanka):
 
     return parsed
 
+def createGraph(n_tokens, u_tokens):
+    graph = {}
+    n_i = 0
+    u_i = 0
+    n_point = 0
+    u_point = 0
+    n_end = False
+    u_end = False
+    while not (n_end and u_end):
+        try:
+            if n_point == u_point:
+                if n_point in graph:
+                    print('error occurd!')
+                    break
+                graph[n_point] = {'n':n_tokens[n_i], 'u':u_tokens[u_i]}
+                n_point += len(n_tokens[n_i][0])
+                u_point += len(u_tokens[u_i][0])
+                n_i += 1
+                u_i += 1
+            elif n_point < u_point:
+                if n_point in graph:
+                    print('error occurd!')
+                    break
+                graph[n_point] = {'n':n_tokens[n_i], 'u':None}
+                n_point += len(n_tokens[n_i][0])
+                n_i += 1
+            elif n_point > u_point:
+                if u_point in graph:
+                    print('error occurd!')
+                    break
+                graph[u_point] = {'n':None, 'u':u_tokens[u_i]}
+                u_point += len(u_tokens[u_i][0])
+                u_i += 1
+        except IndexError:
+            if n_i == len(n_tokens):
+                n_i = n_i - 1
+                n_end = True
+            elif u_i == len(u_tokens):
+                u_i = u_i - 1
+                u_end = True
+
+    if n_point in graph:
+        print('error occurd!')
+
+    graph[n_point] = {'n':None, 'u':None}
+
+    return graph
+
+def merge(n_tokens, u_tokens):
+    token_graph = createGraph(n_tokens, u_tokens)
+    merge_tokens = []
+    priority = 0
+    p = 0
+    while not (token_graph[p]['n'] == None and token_graph[p]['u'] == None):
+        if priority > 1:
+            priority = 0
+        node = token_graph[p]
+
+        if node['u'] == None:
+            merge_tokens.append(node['n'])
+            p += len(node['n'][0])
+            priority = 0
+        elif node['n'] == None:
+            merge_tokens.append(node['u'])
+            p += len(node['u'][0])
+            priority += 1
+        else:
+            if countMora(node['n']) > countMora(node['u']):
+                merge_tokens.append(node['n'])
+                p += len(node['n'][0])
+                priority = 0
+            elif countMora(node['n']) < countMora(node['u']):
+                merge_tokens.append(node['u'])
+                p += len(node['u'][0])
+                priority += 1
+            else:
+                if len(node['n'][0]) > len(node['u'][0]):
+                    merge_tokens.append(node['n'])
+                    p += len(node['n'][0])
+                    priority = 0
+                elif len(node['n'][0]) < len(node['u'][0]):
+                    merge_tokens.append(node['u'])
+                    p += len(node['u'][0])
+                    priority += 1
+                else:
+                    if priority == 0:
+                        merge_tokens.append(node['n'])
+                        p += len(node['n'][0])
+                    else:
+                        merge_tokens.append(node['u'])
+                        p += len(node['u'][0])
+
+    return merge_tokens
+
+def parseFixed2(tanka):
+    m_neologd = init_mecab('/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
+    m_unidic = init_mecab('/usr/lib/x86_64-linux-gnu/mecab/dic/UniDic-qkana_1603')
+    #m_neologd = init_mecab('/usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+    #m_unidic = init_mecab('/usr/local/lib/mecab/dic/UniDic-qkana_1603')
+    tanka = re.sub(r'[ ]+?', '　', tanka)
+    neologd_tokens = tokenize_mecab(tanka, m_neologd)
+    unidic_tokens = tokenize_mecab(tanka, m_unidic, dic='uniDic')
+
+    merge_tokens = merge(neologd_tokens, unidic_tokens)
+
+    return merge_tokens
+
 def main():
     """
     txt_manager = TxtManager()
@@ -164,8 +271,10 @@ def main():
         p = parseFixed(tanka.rstrip())
         print(p)
     """
-    tanka = '一番じゃないんだねもうおはようもおやすみもクロが先なんだね'
-    print(parseFixed(tanka.rstrip()))
+    tanka = 'めつきりとGigaの減つてる月末に飢餓をかんじてすこしひもじい'
+    csv_manager = CsvManager()
+    m = parseFixed2(tanka)
+    csv_manager.make_file(m, '../data/test3.txt')
     """
     m_neologd = init_mecab('/usr/local/lib/mecab/dic/mecab-ipadic-neologd')
     m_unidic = init_mecab('/usr/local/lib/mecab/dic/UniDic-qkana_1603')
